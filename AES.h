d51abbbb3db8f05fd5335c22b4f9dfff8ae1e9d0
Brandon Anderson <Brandon753.ba@gmail.com>
2017-11-26 18:43:00 -0700

    Added Gen key function.

--- AES.h
+++ AES.h
@@ -1,12 +1,17 @@
 #ifndef AES_H
 #define AES_H
 
+#define _GNU_SOURCE
+
+#include <sys/syscall.h>
+#include <linux/random.h>
 #include "common.h"
 #include "AddRoundKey.h"
 #include "mixColumns.h"
 #include "ShiftRows.h"
 #include "subBytes.h"
 
+int genSecureKey(key_t *key, key_size_t key_size);
 int Cipher(block_t *in, block_t *out, key_t *key);
 
 #endif
